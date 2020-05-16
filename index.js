597a95a600a63e7977e5412d033dee35e81b940f
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-05-16 10:27:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+const name=["Ada", "Brendan", "Ali"]
+const events=["birthday"]
+function writeCards(name){
+for (let i=0;i<name.length;i++){
+  console.log("Thank you,${name[i]}, for the wonderful ${events} gift!");
+}
+return name;
+}

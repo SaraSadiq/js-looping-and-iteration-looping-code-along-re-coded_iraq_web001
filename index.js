755b0e4def300e189f2a369d4a6e49100d3c6d89
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-05-16 14:17:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+
 function writeCards(name,events){
   for(let i=0;i<name.length;i++){
     console.log(`Thank you, ${name[i]}, for the wonderful ${events} gift!`);
   }
   return name,events;
 }
-writeCards(name,events)
+writeCards(name,"surprise")
 let countup = 10;
 function countDown(countup){
 while (countup >= 0 ) {

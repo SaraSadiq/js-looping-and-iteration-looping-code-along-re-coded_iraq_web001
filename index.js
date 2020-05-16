38b4f6cad450be4738f22748160913712831a5f9
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-05-16 14:11:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-const names= ["Lisa", "Kaitlin", "Jan"];
-const events="surprise";
-function writeCards(name,events){
-for (let i=0;i<names.length;i++){
-  console.log(`Thank you, ${name[i]}, for the wonderful ${events} gift!`);
-}
-return  (names, events);
-}
-writeCards(names,events)
+const name=[];
 
 let countup = 10;
 function countDown(countup){
